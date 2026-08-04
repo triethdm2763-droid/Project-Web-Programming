dfaa8986227b19f3ff83f875cfcc742c1547beab
caominhtri853-hash <caominhtri853@gmail.com>
2026-08-05 01:35:11 +0700

    test(e2e): add authentication CodeceptJS tests

--- codecept.conf.js
+++ codecept.conf.js
@@ -2,17 +2,20 @@
 export const config = {
   tests: './e2e/*_test.js',
   output: './output',
+
   helpers: {
     Playwright: {
       browser: 'chromium',
-      url: 'http://localhost/Project-Web-Programming-main',
+      url: 'http://localhost/Project-Web-Programming',
       show: true
     }
   },
+
   include: {
     I: './steps_file.js'
   },
-  noGlobals: true,
+
   plugins: {},
-  name: 'Project-Web-Programming-main'
+
+  name: 'Project-Web-Programming'
 }
